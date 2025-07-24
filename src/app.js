8cc703d7ded6b742acd90bced342e9358d1203f5
Oluwasuan Ayanfeoluwa <ayanfe1711@gmail.com>
2025-07-24 22:12:17 +0100

    added frontend v1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 const llmRouter = require('../nichepoint');
 
 app.use(cors());
 app.use(express.json());
 app.use('/v1/llm', llmRouter);
-
+app.use(express.static(__dirname));
 
 module.exports = app;
